ddd20f9ccc88599c60e6e84993ff9bb55041fac0
Brandon <bjpaul99@gmail.com>
2021-12-07 20:26:29 -0700

    Added links

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/journal">Journals</router-link> |
+      <router-link to="/photo">Photos</router-link>
     </div>
     <router-view />
   </div>
