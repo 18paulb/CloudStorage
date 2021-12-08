9b46d9d47213c8ee1dc7731be79d573c75c30462
Brandon <bjpaul99@gmail.com>
2021-12-08 14:40:39 -0700

    Fixed some functions

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -1,35 +1,66 @@
 <template>
   <div>
     <div class='form'>
-      <input v-model='title' placeholder='Title'>
+      <form>
+        <input type='text' v-model='title' placeholder='Title'>
+      </form>
       <br/>
       <label>Photo Title</label>
-      <p></p>
+      <p>{{title}}</p> <!--Testing-->
+
       <textarea v-model='description' placeholder='Description' rows='2' cols='50'></textarea>
-      <p></p>
+      <p>{{description}}</p> <!--Testing-->
       <input type='file' name='photo' @change='fileChanged'>
       <button @click='upload'>Upload</button>
     </div>
+
     <div class='upload' v-if='addItem'>
       <h2>{{addItem.title}}</h2>
       <img :src='addItem.path'/>
     </div>
+
   </div>
 
 </template>
 
 <script>
-//import axios from 'axios'
+import axios from 'axios'
 
 export default {
 
   data() {
     return {
+      title: "",
+      description: "",
+      file: null,
+      url: "",
+      addItem: null,
     }
   },
 
   methods: {
 
+    fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+
+
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name);
+        formData.append('title', this.title);
+        formData.append('description', this.description);
+        await axios.post("/api/photos", formData);
+        this.file = null;
+        this.url = "";
+        this.title = "";
+        this.description = "";
+        this.$emit('uploadFinished');
+      } catch (error) {
+        this.error = "Error: " + error.response.data.message;
+      }
+    }
   },
 
 
