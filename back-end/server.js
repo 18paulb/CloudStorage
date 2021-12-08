74ca1eee8486db65b829c618f4ee8e9c03452c1b
Brandon <bjpaul99@gmail.com>
2021-12-07 20:49:24 -0700

    Schema changes

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 // Configure multer so that it will upload to '../front-end/public/images'
 const multer = require('multer')
+
 const upload = multer({
   dest: '../front-end/public/images/',
   limits: {
