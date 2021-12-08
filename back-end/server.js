f248ee2e4ebb2b7b9d9c0eebfe422a67820fa8a9
Brandon <bjpaul99@gmail.com>
2021-12-07 20:15:55 -0700

    Added schemas

---
+++ back-end/server.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+const mongoose = require('mongoose');
+
+const journalSchema = new mongoose.Schema({
+  name: String,
+  content: String,
+  date: String,
+})
+
+
+const Journal = mongoose.mode('Journal', journalSchema);
+
+
+//No idea but creates journal entry
+app.post('/api/items', async (req, res) => {
+  const journal = new Journal({
+    name: req.body.name,
+    content: req.body.content,
+    date: req.body.date,
+  });
+  try {
+    await journal.save();
+    res.send(journal);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+
+app.get('/api/items', async (req, res) => {
+  try {
+    let journals = await Journal.find();
+    res.send(journals);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Journal.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+
+app.put('api/items/:id', async (req, res) => {
+  try {
+    let journal = await Journal.findOne({
+      _id: req.params.id,
+    });
+    journal.name = req.body.name
+    journal.content = req.body.description
+    journal.data = req.body.date
+    journal.save()
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+app.listen(3000, () => console.log('Kade is trash, and server is listening on port 3000!'));
